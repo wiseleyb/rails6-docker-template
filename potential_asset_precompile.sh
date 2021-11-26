1aacf700ebcb90c0fa479acc380b093b8c475676
Ben Wiseley <wiseleyb@gmail.com>
2021-11-25 16:03:09 -0800

    Add webpacker:react
    Just these commands
    
    ```
    bundle install
    bundle exec rake webpacker:install
    bundle exec rake webpacker:install:react
    ```
    
    from here https://www.honeybadger.io/blog/react-rails/

---
+++ potential_asset_precompile.sh
@@ -0,0 +1,9 @@
+# scripts/potential_asset_precompile.sh
+#!/bin/bash
+
+if [ $# -eq 0 ] || [ $1 != "not" ] ; then
+  echo 'precompiling assets'
+  RAILS_ENV=production rake assets:precompile
+else
+  echo "argument is not, so not running precompile"
+fi
